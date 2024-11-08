c16a294a6ae9aaf1585c80bec0fdc46dc6858453
Adebest <aadebesta@gmail.com>
2024-11-08 14:51:48 +0100

    yea

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -8,10 +8,37 @@ const VerifyEmail = () => {
 console.log(id);
 
   },[])
+const postData = async () => {
+  if (!id) {
+    alert("something is wrong")
+    return 
+  }
+    try {
+    const response = await fetch("https://kryptotradingbackend.com.ng/Api/verify.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify({
+        email:id
+    }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    console.log('Success:', data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+  };
 
   return (
     <div>
       <h1>Verify email </h1>
+      <button onClick={postData} className='bg-blue text-white font-semibold' >Verify</button>
     </div>
   )
 }
